fcd629d2c072d6794b69dc6b474d620562ca327c
crisecys <cristian.uva@studenti.isissgobetti.it>
2024-10-11 09:31:12 +0200

    esercizio svolto 11/10/2024

--- group_by.sql
+++ group_by.sql
@@ -28,6 +28,14 @@ LIMIT 10;
 
 -- Sales volume and receipts by year
 -- See: http://www.sqlite.org/lang_datefunc.html
+SELECT STRFTIME('%Y', invoice_date) AS year,
+       COUNT(*) AS invoice_count,
+       SUM(total) AS invoice_total
+FROM invoices
+GROUP BY year
+ORDER BY year ASC;
+
+
 -- "STRFTIME" means "string format time" and is used to format the output of
 -- dates, times, and timestamps.
 -- See: http://cl.ly/image/0C1m372r233t
@@ -53,28 +61,98 @@ ORDER BY year ASC, month ASC;
 
 -- A list of the top 5 US states by number of invoices
 -- Hint: You'll need to filter the results with WHERE billing_country = 'USA'
+SELECT billing_state, COUNT(*) AS invoice_count
+FROM invoices
+WHERE billing_country = 'USA'
+GROUP BY billing_state
+ORDER BY invoice_count DESC
+LIMIT 5;
+
 
 -- A list of the top 5 US states by gross sales
+SELECT billing_state, SUM(total) AS gross_sales
+FROM invoices
+WHERE billing_country = 'USA'
+GROUP BY billing_state
+ORDER BY gross_sales DESC
+LIMIT 5;
+
 
 -- A list of the top 5 US states by average invoice size
+SELECT billing_state, AVG(total) AS avg_invoice_size
+FROM invoices
+WHERE billing_country = 'USA'
+GROUP BY billing_state
+ORDER BY avg_invoice_size DESC
+LIMIT 5;
+
 
 -- A list of the top 10 US cities by number of invoices
+SELECT billing_city, COUNT(*) AS invoice_count
+FROM invoices
+WHERE billing_country = 'USA'
+GROUP BY billing_city
+ORDER BY invoice_count DESC
+LIMIT 10;
+
 
 -- A list of the top 10 US cities by gross sales
+SELECT billing_city, SUM(total) AS gross_sales
+FROM invoices
+WHERE billing_country = 'USA'
+GROUP BY billing_city
+ORDER BY gross_sales DESC
+LIMIT 10;
+
 
 -- A list of the top 10 US cities by average invoice size
+SELECT billing_city, AVG(total) AS avg_invoice_size
+FROM invoices
+WHERE billing_country = 'USA'
+GROUP BY billing_city
+ORDER BY avg_invoice_size DESC
+LIMIT 10;
+
 
 -- A list of the top 3 cities in California by number of invoices
--- Hint: You'll need a WHERE clause filtering by both billing_country and billing_state
+SELECT billing_city, COUNT(*) AS invoice_count
+FROM invoices
+WHERE billing_country = 'USA' AND billing_state = 'CA'
+GROUP BY billing_city
+ORDER BY invoice_count DESC
+LIMIT 3;
+
 
 -- A list of the top 3 cities in California by gross sales
+SELECT billing_city, SUM(total) AS gross_sales
+FROM invoices
+WHERE billing_country = 'USA' AND billing_state = 'CA'
+GROUP BY billing_city
+ORDER BY gross_sales DESC
+LIMIT 3;
+
 
 -- A list of the top 3 cities in California by average invoice size
+SELECT billing_city, AVG(total) AS avg_invoice_size
+FROM invoices
+WHERE billing_country = 'USA' AND billing_state = 'CA'
+GROUP BY billing_city
+ORDER BY avg_invoice_size DESC
+LIMIT 3;
 
-
--- "customers" table
--- Remember: run ".schema customers" to see what fields (columns) the customers table contains.
+-- Queries for the "customers" table
 
 -- A list of the top 3 countries by total number of customers
+SELECT country, COUNT(*) AS customer_count
+FROM customers
+GROUP BY country
+ORDER BY customer_count DESC
+LIMIT 3;
 
--- A list of the top 7 cities (anywhere) by total number of customers
+
+-- A list of the top 7 cities by total number of customers
+SELECT city, COUNT(*) AS customer_count
+FROM customers
+GROUP BY city
+ORDER BY customer_count DESC
+LIMIT 7;
